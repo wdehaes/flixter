b2654aa68b83e88eea9c1dd1870db1e8cc514fae
Willem Dehaes <willemdehaes@gmail.com>
2016-07-16 09:51:38 +0000

    sticky footer (11)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Flixter::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #devise
+  config.action_mailer.default_url_options = { host: 'flixter-willem-dehaes.herokuapp.com' }
 end
