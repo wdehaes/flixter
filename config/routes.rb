e963193f970b8f755840b45a319867d3396d5f72
Willem Dehaes <willemdehaes@gmail.com>
2016-07-22 11:58:15 +0000

    student enrollment (26)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Flixter::Application.routes.draw do
   
   root 'static_pages#index'
   devise_for :users
-  resources :courses, only: [:index, :show]
+  resources :courses, only: [:index, :show] do
+    resources :enrollments, only: :create
+  end
   resources :lessons, only: [:show]
   namespace :instructor do
     resources :sections, only: [] do
