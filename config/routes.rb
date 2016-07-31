34f5a2d7fc8b6d1e78cf1c8933273be375ec74b5
Willem Dehaes <willemdehaes@gmail.com>
2016-07-31 09:50:43 +0000

    instructor dashboard (33)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Flixter::Application.routes.draw do
   
   root 'static_pages#index'
   devise_for :users
+  resource :dashboard, only: [:show]
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
