0d7fb0510e007298fb37cbeda0ae172737bcbb8c
Willem Dehaes <willemdehaes@gmail.com>
2016-08-01 19:26:58 +0000

    bug when email or pw is incorrect at log-in

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:email]
 
   def enrolled_in?(course)
     return enrolled_courses.include?(course)
